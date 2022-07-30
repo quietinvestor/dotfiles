8efc139c71161e8eb5f9dc317138a1434d6719af
quietinvestor <4750936+quietinvestor@users.noreply.github.com>
2022-07-30 13:09:19 +0200

    Set default tab and indent to 4 spaces

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -5,5 +5,7 @@ local o = vim.o
 
 -- global options
 o.cursorline = true -- Highlight selected line
+o.tabstop = 4 -- Use 4 spaces when tab is pressed
+o.shiftwidth = 4 -- Use 4 spaces for indentation
 o.expandtab = true -- Spaces are used in indents
 o.number = true -- Add line numbers
